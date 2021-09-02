a3560d51d7fee3cc76614ff113d1223ca6e196a4
Jere Jokelainen <jerejokelainen@gmail.com>
2021-09-02 16:49:38 +0300

    boilerplate for the issue

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
